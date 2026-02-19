7ff2dd7375c64b24f7d1f73cdf844d632b00262c
LZimmer <zimmer.lucas.w@gmail.com>
2026-02-19 14:56:06 +0100

    Remove old file, add run script for instance opt

---
+++ Code/run_instance_optimization.sh
@@ -0,0 +1,2 @@
+#CUDA_VISIBLE_DEVICES=1 python BRATS_instance_optimization.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ --test_run > output_inst_opt.txt 2>&1 &
+CUDA_VISIBLE_DEVICES=5 python BRATS_instance_optimization.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ > output_inst_opt.txt 2>&1 &
