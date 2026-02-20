ba3c41bb66b952ef93a2542b03f13de58ff0ced0
LZimmer <zimmer.lucas.w@gmail.com>
2026-02-20 17:16:31 +0100

    Add example outputs

--- Code/run_instance_optimization.sh
+++ Code/run_instance_optimization.sh
@@ -1,2 +1,4 @@
 #CUDA_VISIBLE_DEVICES=1 python BRATS_instance_optimization.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ --test_run > output_inst_opt.txt 2>&1 &
+#CUDA_VISIBLE_DEVICES=1 python BRATS_instance_optimization_alt.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ --test_run > output_inst_opt_alt.txt 2>&1 &
 CUDA_VISIBLE_DEVICES=5 python BRATS_instance_optimization.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ > output_inst_opt.txt 2>&1 &
+CUDA_VISIBLE_DEVICES=1 python BRATS_instance_optimization_alt.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ > output_inst_opt_alt.txt 2>&1 &
