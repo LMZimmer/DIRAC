3d2ee94ed1d84288a7c2df790d6b350fddb6d486
LZimmer <zimmer.lucas.w@gmail.com>
2026-04-29 15:48:35 +0200

    Testing

--- Code/run_transforms.sh
+++ Code/run_transforms.sh
@@ -1,2 +1,2 @@
-#CUDA_VISIBLE_DEVICES=1 python BRATS_apply_longitudinal_warp.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ --test_run
-CUDA_VISIBLE_DEVICES=1 python BRATS_apply_longitudinal_warp.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ > output_transform.txt 2>&1 &
+CUDA_VISIBLE_DEVICES=1 python BRATS_apply_longitudinal_warp.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ --test_run
+#CUDA_VISIBLE_DEVICES=1 python BRATS_apply_longitudinal_warp.py --datapath /mnt/Drive2/lucas/models/DIRAC/Dataset/predict_gbm/ > output_transform.txt 2>&1 &
